d55fb03d01bc070c3a9bb8a6e8af333ccde4f522
Rupesh155 <rudhg35@gmail.com>
2024-11-20 14:58:42 +0530

    api

--- lect-6/index.js
+++ lect-6/index.js
@@ -18,14 +18,53 @@
 // })
 
 // })
-let h2=  document.querySelector('h2')
-let p=  document.querySelector('p')
-let img=  document.querySelector('img')
+
 fetch('https://dummyjson.com/recipes').then((res)=>{
     return res.json()
 
 }).then((data)=>{
-    console.log(data.recipes,"hehehehe");
-    img.setAttribute('src',data.recipes[0].image)
-    h2.innerText=data.recipes[0].name
+    api(data.recipes)
+    // console.log(data.recipes,"hehehehe");
+  
+// data.recipes.map((val)=>{
+//     console.log(val,"heheheheh");
+    
+
+//     img.setAttribute('src',)
+    
+// })
+
+//     img.setAttribute('src',data.recipes[0].image)
+//     h2.innerText=data.recipes[0].name
 })
+
+
+
+
+function api(apiData){
+    let div=  document.querySelector('div')
+    console.log(apiData,"helloo");
+    for(let val of apiData){
+    //  let img=    document.createElement('img')
+    // document.querySelector()
+            let h2=  document.createElement('h2')
+    // let p=  document.querySelector('p')
+
+    img.setAttribute('src', val.image)
+    h2.innerText=val.name
+    div.append(img,h2)
+
+    }
+
+    
+
+}
+
+
+
+
+// sum(5)
+// function sum(a)
+// {
+
+// }
