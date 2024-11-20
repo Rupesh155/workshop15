e8f83c8634441c4a7ad45542f4b909373cf96ba3
Rupesh155 <rudhg35@gmail.com>
2024-11-20 13:27:24 +0530

    api

--- lect-6/index.js
+++ lect-6/index.js
@@ -1,12 +1,22 @@
-
-let apiKey='9f6290d6cda9a36a63755fadee71f83d'
-let city='jabalpur'
+// https://github.com/Rupesh155/workshop15
+let btn=  document.querySelector('button')
+let inp=  document.querySelector('input')
+let h2=  document.querySelector('h2')
+let p=  document.querySelector('p')
+btn.addEventListener('click',()=>{
+    let apiKey='9f6290d6cda9a36a63755fadee71f83d'
+let city=inp.value
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).
 then((res)=>{
     return res.json()
 
 }).then((data)=>{
     console.log(data,"heheh");
+    h2.innerText=data.name
+    p.innerText=data.main.temp
     
 
 })
+
+})
+
