1048bec813f82052a9591ad4c8cff7c875ee1bcf
Rupesh155 <rudhg35@gmail.com>
2024-11-25 14:50:47 +0530

    useState

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -48,31 +48,86 @@
 // export default App
 
 
-import React from 'react'
-// import Home from './Home'
-import Card from './Card'
-const App = ({a}) => {
-  // let user='hellooo'
-  let arr=[1,2,3,4,5,6,78]
+// import React from 'react'
+// // import Home from './Home'
+// import Card from './Card'
+// const App = ({a}) => {
+//   // let user='hellooo'
+//   let arr=[1,2,3,4,5,6,78]
+//   return (
+//     <div>
+
+//   {
+//     arr.map((a)=>{
+//       return(<div>
+//         <lI>  {a}</lI>
+
+//       </div>
+//         )
+//     })
+//   }
+
+//        </div>
+       
+//   )
+// }
+
+// export default App
+
+
+
+// import React, { useState } from 'react'
+// const App = () => {
+//   let [samay,SetSamay]=useState()
+//   setInterval(()=>{
+//     let time=    new Date().toLocaleTimeString()
+//     SetSamay(time)
+//   })
+//   return (
+//     <div>
+//       <h2>{samay}</h2>
+//       <button>stop</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+import React, { useEffect, useState } from 'react'
+const App = () => {
+  // console.log('helloooooo');
+  
+
+  useEffect(()=>{
+
+  fetch('https://dummyjson.com/recipes').then((res)=>{
+    return res.json()
+
+  }).then((data)=>{
+    console.log(data);
+    
+
+  })
+
+  },[])
+
+  
+  let [count,SetCount]=useState(0)
+  function fun1(){
+    SetCount(count+1)
+  }
   return (
     <div>
-
-  {
-    arr.map((a)=>{
-      return(<div>
-        <lI>  {a}</lI>
-
-      </div>
-        )
-    })
-  }
-
-       </div>
-       
+      <h2> {count}</h2>
+      <button onClick={fun1}> click</button>
+    </div>
   )
 }
 
 export default App
+
 
 
 
